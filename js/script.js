cb72b61a746440f34531933dcb65fb8f8545b05f
Glitch (rock-p) <none>
2021-12-02 00:10:02 +0000

    I created a Math.random generator and mad it choose a number between 0 and 3. I also used a Math.ceil to round it up to the nearest whole number. I didn't want it to only display the number so depending on the number the generator picked, it paired with either rock, paper or scisors.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ $(".play").click(function(){
  $(".userChoice").text(userInput)
 });
 
+$(".play").click(function(){
+  let compChoice =Math.ceil(Math.random()*3);
+  
+  if( compChoice === 1){
+    $(".computerChoice").text(`Rock`)
+  }else if (compChoice ===2){
+    $(".computerChoice").text(`Paper`)
+  } else if (compChoice ===3){
+    $(".computerChoice").text(`Scissors`)
+  }  
+});
