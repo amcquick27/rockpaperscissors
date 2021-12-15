152c6a6826f6751b21fc096d25d97660a5c4f127
Glitch (rock-p) <none>
2021-12-15 23:28:44 +0000

    I added a conditional that tells the user if they tied with the computer.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 $(".play").click(function(){
   let userInput = $(".input").val();
- $(".userChoice").text(userInput)
-});
+  $(".userChoice").text(userInput)
 
-$(".play").click(function(){
   let compChoice =Math.ceil(Math.random()*3);
-  
+  let compChoiceWord;
   if( compChoice === 1){
     $(".computerChoice").text(`Rock`)
+    compChoiceWord = 'rock';
   }else if (compChoice ===2){
     $(".computerChoice").text(`Paper`)
+    compChoiceWord = 'paper'
   } else if (compChoice ===3){
     $(".computerChoice").text(`Scissors`)
-  }  
+    compChoiceWord ='scissors'
+  } 
+  
+  if (userInput.toLowerCase() === compChoiceWord) {
+    $(".result").text("Tie!");
+  }
+  
+  if ((userInput.toLowerCase() === 'rock' && compChoiceWord==='rock') || (userInput.toUpperCase === 'Paper' && compChoiceWord==='Paper') || (userInput.toUpperCase === 'Scissors' && compChoiceWord==='Scissors')) {
+    $(".result").text("Tie!")
+  }
 });
