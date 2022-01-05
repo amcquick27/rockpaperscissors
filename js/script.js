5a9b721be2cb895e8c5db552dda113b8a7b5c58c
Glitch (rock-p) <none>
2022-01-05 23:21:29 +0000

    I was able to create a function for assigning the computer choice.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 $(".play").click(function(){
   let userInput = $(".input").val();
   $(".userChoice").text(userInput)
-
-  let compChoice =Math.ceil(Math.random()*3);
-  let compChoiceWord;
-  if( compChoice === 1){
-    $(".computerChoice").text(`Rock`)
-    compChoiceWord = 'rock';
-  }else if (compChoice ===2){
-    $(".computerChoice").text(`Paper`)
-    compChoiceWord = 'paper'
-  } else if (compChoice ===3){
-    $(".computerChoice").text(`Scissors`)
-    compChoiceWord ='scissors'
-  } 
   
-  if (userInput.toLowerCase() === compChoiceWord) {
-    $(".result").text("Tie!");
-  }
-  
-  if ((userInput.toLowerCase() === 'rock' && compChoiceWord==='rock') || (userInput.toUpperCase === 'Paper' && compChoiceWord==='Paper') || (userInput.toUpperCase === 'Scissors' && compChoiceWord==='Scissors')) {
+  let compChoiceWord = getCompChoice();
+  if ((userInput.toLowerCase() === 'rock' && compChoiceWord === 'scissors') || (userInput.toLowerCase() === 'paper' && compChoiceWord==='rock') ||  userInput.toLowerCase() === 'scissors' && compChoiceWord==='paper') {
+    $(".result").text("You Win");
+  } else if ((userInput.toLowerCase() === 'rock' && compChoiceWord==='rock') || (userInput.toLowerCase === 'paper' && compChoiceWord==='paper') || (userInput.toLowerCase === 'scissors' && compChoiceWord==='scissors')) {
+    $(".result").text("Tie!")
+  }else if ((userInput.toLowerCase() === 'rock' && compChoiceWord==='paper') || (userInput.toLowerCase === 'paper' && compChoiceWord==='scissors') || (userInput.toLowerCase === 'scissors' && compChoiceWord==='rock')) {
     $(".result").text("Tie!")
   }
 });
+
+function getCompChoice(){
+  let compChoice =Math.ceil(Math.random()*3);
+  if( compChoice === 1){
+    $(".computerChoice").text(`rock`)
+   return 'rock';
+  }else if (compChoice ===2){
+    $(".computerChoice").text(`paper`)
+    return 'paper'
+  } else if (compChoice ===3){
+    $(".computerChoice").text(`scissors`)
+    return 'scissors'
+  } 
+}
